075511df172502e706c2f4a0ae006223754588e6
renjiem <renjiem@student.unimelb.edu.au>
2019-04-20 10:08:47 +1000

    Commit before branching

--- handle_http_request.c
+++ handle_http_request.c
@@ -1,10 +1,17 @@
 #include "handle_http_request.h"
 
-bool send_page_to_user(char* page_to_sent, char* buff, int n, int sockfd, int num_bit_add){
+bool send_page_to_user(char* page_to_sent, char* buff, int n, int sockfd, int num_bit_add, bool does_send_cookie){
     // get the size of the file
     struct stat st;
     stat(page_to_sent, &st);
-    n = sprintf(buff, HTTP_200_FORMAT_WITH_COOKIE, st.st_size + num_bit_add, 0);
+
+    // response with Set-cookie
+    if (does_send_cookie){
+        n = sprintf(buff, HTTP_200_FORMAT_WITH_COOKIE, st.st_size + num_bit_add, 0);
+    // response without Set-cookie
+    }else{
+        n = sprintf(buff, HTTP_200_FORMAT, st.st_size + num_bit_add, 0);
+    }
     // send the header first
     if (write(sockfd, buff, n) < 0)
     {
@@ -109,29 +116,33 @@ bool handle_http_request(int sockfd)
         if (method == GET)
         {
             char* page_to_sent;
+            bool does_send_cookie;
             // GET HOME_PAGE
             if (is_GET_HOME_PAGE(curr)){
                 page_to_sent = (char*)malloc(sizeof(HOME_PAGE));
                 strncpy(page_to_sent, HOME_PAGE, HOME_PAGE_PATH_LENGTH);
+                does_send_cookie = SEND_COOKIE;
             // GET GAME_PLAYING_PAGE
             }else if(is_GET_GAME_PLAYING_PAGE(curr)){
                 page_to_sent = (char*)malloc(sizeof(GAME_PLAYING_PAGE));
                 strncpy(page_to_sent, GAME_PLAYING_PAGE, GAME_PLAYING_PAGE_PATH_LENGTH);
+                does_send_cookie = NOT_SEND_COOKIE;
             }else if(is_GET_FAV_ICON(curr)){
                 page_to_sent = (char*)malloc(sizeof(FAV_ICON));
                 strncpy(page_to_sent, FAV_ICON, FAV_ICON_PATH_LENGTH);
+                does_send_cookie = NOT_SEND_COOKIE;
             }
 
             printf("%s\n", page_to_sent);
             // send the page which the client request to client
-            send_page_to_user(page_to_sent, buff, n, sockfd, NOTHING_TO_ADD);
+            send_page_to_user(page_to_sent, buff, n, sockfd, NOTHING_TO_ADD, does_send_cookie);
             free(page_to_sent);
         }
         else if (method == POST)
         {
             // quit POST
             if (is_QUIT(buff)){
-                send_page_to_user(GAME_OVER_PAGE, buff, n, sockfd, NOTHING_TO_ADD);
+                send_page_to_user(GAME_OVER_PAGE, buff, n, sockfd, NOTHING_TO_ADD, NOT_SEND_COOKIE);
                 close(sockfd);
             // username POST
             }else if(is_SUBMIT_Username(buff)){
@@ -144,7 +155,7 @@ bool handle_http_request(int sockfd)
 
 
                 // send the modified Main Page
-                send_page_to_user(MAIN_PAGE, buff, n, sockfd, len_of_thing_to_add);
+                send_page_to_user(MAIN_PAGE, buff, n, sockfd, len_of_thing_to_add, NOT_SEND_COOKIE);
                 // Add username into the main page
                 if (write(sockfd, "<p/>", 4) < 0)
                 {
@@ -164,7 +175,7 @@ bool handle_http_request(int sockfd)
                 // keyword discard
                 // game completed
 
-                send_page_to_user(KEYWORD_ACCEPTED_PAGE, buff, n, sockfd, NOTHING_TO_ADD);
+                send_page_to_user(KEYWORD_ACCEPTED_PAGE, buff, n, sockfd, NOTHING_TO_ADD, NOT_SEND_COOKIE);
             }
 
         }
